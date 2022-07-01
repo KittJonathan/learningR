e04c106623ee1cefd08205ff396052c425413946
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-01 14:53:53 +0200

    testing ggplot2 code in r4ds code

--- R/r4ds.R
+++ R/r4ds.R
@@ -7,3 +7,22 @@
 # Load packages ----
 
 library(tidyverse)
+
+# Data visualisation ----
+
+# https://r4ds.had.co.nz/data-visualisation.html
+
+# The mpg data frame
+mpg
+?mpg
+
+# Creating a ggplot
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy),
+             colour = "blue")
