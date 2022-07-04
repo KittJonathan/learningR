d5c707ad978ce0b19204008d31a23068f1ae53ab
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-04 10:46:58 +0200

    finished 3.6 paragraph

--- R/r4ds.R
+++ R/r4ds.R
@@ -56,3 +56,52 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_smooth(mapping = aes(x = displ, y = hwy,
                             linetype = drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+mpg %>% 
+  ggplot() +
+  geom_smooth(mapping = aes(x = displ, y = hwy, colour = drv),
+              show.legend = FALSE)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg,
+       mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth()
+
+ggplot(data = mpg,
+       mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(colour = class)) +
+  geom_smooth(data = filter(mpg, class == "subcompact"),
+              se = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+ggplot() + 
+  geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg,
+       mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(fill = drv),
+             colour = "white",
+             stroke = 2,
+             shape = 21, size = 3)
+
+# Statistical transformations 
+
+# https://r4ds.had.co.nz/data-visualisation.html#statistical-transformations
