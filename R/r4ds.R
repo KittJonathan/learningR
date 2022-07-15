c7dd0d382d3796c8f12ea781d5360da352266b3b
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-07-15 11:55:03 +0200

    finished 3rd chapter

--- R/r4ds.R
+++ R/r4ds.R
@@ -165,3 +165,31 @@ ggplot(data = mpg) +
 
 # Coordinate systems
 
+ggplot(data = mpg, 
+       mapping = aes(x = class, y = hwy)) +
+  geom_boxplot()
+
+ggplot(data = mpg, 
+       mapping = aes(x = class, y = hwy)) +
+  geom_boxplot() +
+  coord_flip()
+
+nz <- map_data("nz")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", colour = "black")
+
+ggplot(nz, aes(long, lat, group = group)) +
+  geom_polygon(fill = "white", colour = "black") +
+  coord_quickmap()
+
+bar <- ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = cut, fill = cut),
+           show.legend = FALSE,
+           width = 1) +
+  theme(aspect.ratio = 1) +
+  labs(x = NULL, y = NULL)
+
+bar
+bar + coord_flip()
+bar + coord_polar()
